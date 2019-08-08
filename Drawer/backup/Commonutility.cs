4338a38192b225bf1b814ba8a68b084dc6d3d0d9
SuuuperLeo <suuuperleoliu@gmail.com>
2019-08-08 16:39:12 +0800

    init

---
+++ Drawer/backup/Commonutility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts
+{
+    static class Commonutility
+    {
+        public static void Reverse<T>(this T[] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                var tmp = array[i];
+                array[i] = array[array.Length - 1 - i];
+                array[array.Length - 1 - i] = tmp;
+            }
+        }
+    }
+}
